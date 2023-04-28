0077b2cd04516aa8355449339c22e3109f512b91
Farhan Jiwani <farhan@farhanjiwani.com>
2023-04-28 17:57:20 -0400

    feat: initial commit - minified code for bookmarklet

---
+++ README.md
@@ -0,0 +1,52 @@
+# QuickSpeedTTV
+
+A bookmarklet that adds one-keypress video playback speed settings for VODs on [Twitch.tv](https://twitch.tv/) using a keyboard's number pad.
+
+| Numpad Key | Playback Speed |
+| :--------- | -------------: |
+| Numpad 0   |             1x |
+| Numpad 1   |          1.25x |
+| Numpad 2   |           1.5x |
+| Numpad 3   |             2x |
+
+## But Why?
+
+I made this when I just had a newborn and it was hard for me to find any time to play video games, let alone do [my regular Twitch streams](https://twitch.tv/AlfaTrion) so I had to resort to watching VODs of my friends playing games. The only time I ended up getting to watch them was during late night feeds for the baby.
+
+Sometimes if the game was grindy or the streamer wasn't speaking or no story elements were happening, I'd want to not waste precious VOD watching time so I would want to speed things up at those times. Using the mouse (or even tabbing around with the keyboard) was too cumbersome, especially for a starving newborn who would barely make it 1 second w/o crying if I took the bottle away, so I came up with this idea.
+
+## How to Use
+
+1. Create a new bookmark in your web browser of choice which allows bookmarklets, such as Google Chrome.
+1. Give the bookmark a name.
+    - e.g. `script: QuickSpeedTTV`
+1. For the URL, type `javascript:` as the prefix and paste the entire script after that. Now it's a bookmarklet!
+1. Navigate Twitch to a VOD (video on demand) page.
+1. Activate your bookmarklet as you would normally use a bookmark.
+1. Use the number pad keys as indicated in the table above.
+1. Follow me on Twitch (https://twitch.tv/AlfaTrion) and let me know how you like it **OR**
+1. Report any issues you find.
+    - Please be detailed so I can try to recreate the same issue you are having!
+
+## Future Updates
+
+Originally my plan was to make this a browser extension but I barely have time to code so I quickly made this script and thought a bookmarklet is good for now.
+
+### Current Roadmap
+
+-   Minified version
+-   Fix jumpiness outside of full-screen view and theatre view
+
+### Ideal Features
+
+-   YouTube version (although `Shift + >` already works well there)
+-   Chrome and Firefox browser extension w/configurable settings
+-   Error handling
+-   Less verbose in the console logs
+-   Add slower speed settings
+-   Add icon to player controls to show script is running
+-   A way to disable the script w/o refreshing the page
+
+## Known Issues
+
+-   When pressing keys outside of full screen or theatre view modes, the VOD page may jump or scroll down.
---
+++ index.js
@@ -0,0 +1,88 @@
+(() => {
+    // Utilities //
+    /**
+     * Prepend logs and help minification.
+     * TODO: Add flag for debug mode only.
+     * @param {String} msg - Log message.
+     */
+    const log = (msg) => {
+        console.log('[QuickSpeedTTV]', msg);
+    };
+
+    /**
+     * Minification aid.
+     */
+    const dqs = (selector) => document.querySelector(selector);
+
+    // DOM Related Variables.
+    const selectorPrefix = '[data-a-target="player-settings-';
+    const selectors = {
+        btnSettings: `${selectorPrefix}button"]`,
+        menuItemSpeed: `${selectorPrefix}menu-item-speed"]`,
+        subMenuSpeed: `${selectorPrefix}menu"]`,
+    };
+    const $btnVodSettings = dqs(selectors.btnSettings);
+
+    // Setup/Config.
+    const keyCodePrefix = 'Numpad'; // i.e. Numpad0
+    const arrAllowedKeys = Array.from(Array(4).keys()); // 0-3 on numpad
+    const speedIndexPadding = 3; // To ignore the slower speed menu items
+
+    // Event Logic //
+    // Event Listener
+    function addListeners() {
+        log('Adding event listeners...');
+        document.addEventListener('keyup', (e) => {
+            log('Key pressed:', e.code);
+            handleKeyup(e.code.split(keyCodePrefix));
+        });
+    }
+
+    /**
+     * Event handler for onKeyup.
+     * Initiates action only if a valid key on a numpad was pressed.
+     * @param {Array} arrPressed - Pre-split keyCode w/2nd index 2 containing
+     *      the desired menu item list location.
+     */
+    function handleKeyup(arrPressed) {
+        if (arrPressed.length !== 2) return false;
+
+        const speed = parseInt(arrPressed[1]);
+        if (arrAllowedKeys.includes(speed)) {
+            log('Valid key pressed!');
+            setSpeed(speed);
+        }
+    }
+
+    // Action Logic //
+    /**
+     * Simulates selecting the desired speed setting then closes settings menu.
+     * @param {number} speedIndex - Menu item location of desired speed.
+     */
+    function setSpeed(speedIndex) {
+        log(`Setting speed to ${speedIndex}.`);
+        speedIndex += speedIndexPadding;
+        getSpeeds()[speedIndex].click();
+        $btnVodSettings.click();
+    }
+
+    /**
+     * Retrieves the list of available speeds at which the VOD can be played.
+     * @returns {NodeList} - DIVs containing radio buttons for each speed.
+     */
+    function getSpeeds() {
+        log('Getting speeds...');
+        openSpeedMenu();
+        return dqs(selectors.subMenuSpeed).children;
+    }
+
+    // Simulates clicking Settings then Speed.
+    function openSpeedMenu() {
+        log('Opening speed settings...');
+        $btnVodSettings.click();
+        dqs(selectors.menuItemSpeed).click();
+    }
+
+    // Init.
+    addListeners();
+})();
